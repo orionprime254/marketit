7a634771f737826bc76bcbe2dfbcb55a6a7d13ab
Brian Amatalo <brianamatalo34@gmail.com>
2024-09-27 12:53:47 +0300

    bakia location streamlining

--- lib/components/textfield.dart
+++ lib/components/textfield.dart
@@ -3,7 +3,7 @@ class MyTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
   final bool obscureText;
-  const MyTextField({super.key, required this.controller, required this.hintText, required this.obscureText});
+  const  MyTextField({super.key, required this.controller, required this.hintText, required this.obscureText});
 
   @override
   Widget build(BuildContext context) {
